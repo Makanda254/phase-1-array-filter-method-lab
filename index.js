be4d2d51d20ca678a2c17f93815c016cd337109f
Victor John Makanda <victormakanda254@gmail.com>
2023-10-01 21:53:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+let name='';
+
+function findMatching(drivers, name){
+    const lowerCaseName = name.toLowerCase();
+    const newArray= drivers.filter(driver => driver.toLowerCase() === lowerCaseName)
+
+    return newArray;
+
+}
+
+
+
+function fuzzyMatch(drivers, firstLetters=''){
+    
+    const newDrivers = drivers.filter(drivers => drivers.startsWith(firstLetters))
+
+    return newDrivers;
+}
+console.log(fuzzyMatch(drivers, 'Sa'))
+
+function matchName(drivers, driverName=''){
+    return drivers.filter(driver => driver.name === driverName);
+}
+
